454a253304f752a135f90788f7c33081a380bd66
Nikita Koynov <nikicha03@gmail.com>
2019-09-14 11:07:46 +0300

    StudentSystem Db Exercise

---
+++ Databases - Advanced/05. EntityRelations/P01_StudentSystem/Data/StudentSystemContext.cs
@@ -0,0 +1,184 @@
+﻿namespace P01_StudentSystem.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using System;
+
+    using Models;
+
+    public class StudentSystemContext : DbContext
+    {
+        public StudentSystemContext()
+            : base()
+        {
+        }
+
+        public StudentSystemContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<Course> Courses { get; set; }
+
+        public DbSet<Resource> Resources { get; set; }
+
+        public DbSet<Homework> HomeworkSubmissions { get; set; }
+
+        public DbSet<StudentCourse> StudentCourses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer("Server=DESKTOP-RBLV7OL;Database=StudentSystem;Integrated Security=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            this.OnStudentCreating(modelBuilder);
+
+            this.OnCourseCreating(modelBuilder);
+
+            this.OnResourceCreating(modelBuilder);
+
+            this.OnHomeworkCreating(modelBuilder);
+
+            this.OnStudentCourseCreating(modelBuilder);
+        }
+
+        private void OnStudentCourseCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+               .Entity<StudentCourse>()
+               .ToTable("StudentsCourses");
+
+            modelBuilder
+                .Entity<StudentCourse>()
+                .HasKey(sc => new { sc.StudentId, sc.CourseId });
+
+            modelBuilder
+                .Entity<StudentCourse>()
+                .HasOne(sc => sc.Student)
+                .WithMany(s => s.CourseEnrollments)
+                .HasForeignKey(sc => sc.StudentId);
+
+            modelBuilder
+                .Entity<StudentCourse>()
+                .HasOne(sc => sc.Course)
+                .WithMany(c => c.StudentsEnrolled)
+                .HasForeignKey(sc => sc.CourseId);
+        }
+
+        private void OnHomeworkCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Homework>()
+                .ToTable("Homeworks");
+
+            modelBuilder
+                .Entity<Homework>()
+                .HasKey(h => h.HomeworkId);
+
+            modelBuilder
+                .Entity<Homework>()
+                .Property(h => h.Content)
+                .IsUnicode(false)
+                .IsRequired();
+
+            modelBuilder
+                .Entity<Homework>()
+                .HasOne(h => h.Student)
+                .WithMany(s => s.HomeworkSubmissions)
+                .HasForeignKey(h => h.StudentId);
+
+            modelBuilder
+                .Entity<Homework>()
+                .HasOne(h => h.Course)
+                .WithMany(c => c.HomeworkSubmissions)
+                .HasForeignKey(h => h.CourseId);
+        }
+
+        private void OnResourceCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Resource>()
+                .ToTable("Resources");
+
+            modelBuilder
+                .Entity<Resource>()
+                .HasKey(r => r.ResourceId);
+
+            modelBuilder
+                .Entity<Resource>()
+                .Property(r => r.Name)
+                .HasMaxLength(50)
+                .IsUnicode()
+                .IsRequired();
+
+            modelBuilder
+                .Entity<Resource>()
+                .Property(r => r.Url)
+                .IsUnicode(false)
+                .IsRequired();
+
+            modelBuilder
+                .Entity<Resource>()
+                .HasOne(r => r.Course)
+                .WithMany(c => c.Resources)
+                .HasForeignKey(r => r.CourseId);
+        }
+
+        private void OnCourseCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                   .Entity<Course>()
+                   .ToTable("Courses");
+
+            modelBuilder
+                .Entity<Course>()
+                .HasKey(c => c.CourseId);
+
+            modelBuilder
+                .Entity<Course>()
+                .Property(c => c.Name)
+                .HasMaxLength(80)
+                .IsUnicode()
+                .IsRequired();
+
+            modelBuilder
+                .Entity<Course>()
+                .Property(c => c.Description)
+                .IsUnicode();
+        }
+
+        private void OnStudentCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+               .Entity<Student>()
+               .ToTable("Students");
+
+            modelBuilder
+                .Entity<Student>()
+                .HasKey(s => s.StudentId);
+
+            modelBuilder
+                .Entity<Student>()
+                .Property(s => s.Name)
+                .HasMaxLength(100)
+                .IsUnicode()
+                .IsRequired();
+
+            modelBuilder
+                .Entity<Student>()
+                .Property(s => s.PhoneNumber)
+                .HasMaxLength(10)
+                .IsFixedLength()
+                .IsUnicode(false)
+                .IsRequired(false);
+        }
+    }
+}
---
+++ Databases - Advanced/05. EntityRelations/P01_StudentSystem/StartUp.cs
@@ -0,0 +1,188 @@
+﻿namespace P01_StudentSystem
+{
+    using System;
+
+    using Data;
+    using Data.Models;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            using (var context = new StudentSystemContext())
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+
+                SeedCourses(context);
+                SeedResources(context);
+                SeedStudents(context);
+                SeedStudentCourses(context);
+
+            }
+        }
+
+        private static void SeedStudentCourses(StudentSystemContext context)
+        {
+            StudentCourse[] studentCourses =
+            {
+                new StudentCourse
+                {
+                    StudentId = 1,
+                    CourseId = 1
+                },
+                new StudentCourse
+                {
+                    StudentId = 2,
+                    CourseId = 1
+                },
+                new StudentCourse
+                {
+                    StudentId = 3,
+                    CourseId = 4
+                },
+                new StudentCourse
+                {
+                    StudentId = 4,
+                    CourseId = 1
+                },
+                new StudentCourse
+                {
+                    StudentId = 1,
+                    CourseId = 3
+                },
+                new StudentCourse
+                {
+                    StudentId = 2,
+                    CourseId = 3
+                },
+                new StudentCourse
+                {
+                    StudentId = 3,
+                    CourseId = 3
+                }
+            };
+
+            context.StudentCourses.AddRange(studentCourses);
+            context.SaveChanges();
+        }
+
+        private static void SeedStudents(StudentSystemContext context)
+        {
+            Student[] students =
+            {
+                new Student
+                {
+                    Name = "Vasko",
+                    RegisteredOn = DateTime.Now,
+                },
+                new Student
+                {
+                    Name = "Marin",
+                    RegisteredOn = DateTime.Now,
+                },
+                new Student
+                {
+                    Name = "Svetlio",
+                    RegisteredOn = DateTime.Now,
+                },
+                new Student
+                {
+                    Name = "Babcho",
+                    RegisteredOn = DateTime.Now,
+                },
+                new Student
+                {
+                    Name = "Marin2",
+                    RegisteredOn = DateTime.Now,
+                },
+                new Student
+                {
+                    Name = "Asen",
+                    RegisteredOn = DateTime.Now,
+                },
+                new Student
+                {
+                    Name = "Pehlivan",
+                    RegisteredOn = DateTime.Now,
+                }
+            };
+
+            context.Students.AddRange(students);
+            context.SaveChanges();
+        }
+
+        private static void SeedResources(StudentSystemContext context)
+        {
+            Resource[] resources =
+            {
+                new Resource
+                {
+                    Name = "Video for programmers",
+                    ResourceType = ResourceType.Video,
+                    Url = "comming soon",
+                    CourseId = 1
+                },
+                new Resource
+                {
+                    Name = "Presentation for programmers",
+                    ResourceType = ResourceType.Presentation,
+                    Url = "comming soon",
+                    CourseId = 1
+                },
+                new Resource
+                {
+                    Name = "Presentation for new programmers",
+                    ResourceType = ResourceType.Presentation,
+                    Url = "comming soon",
+                    CourseId = 1
+                }
+            };
+
+            context.Resources.AddRange(resources);
+            context.SaveChanges();
+        }
+
+        private static void SeedCourses(StudentSystemContext context)
+        {
+            Course[] courses =
+            {
+                new Course
+                {
+                    Name = "DB ez",
+                    Description = "very easy course",
+                    Price = 1100,
+                    StartDate = DateTime.Now,
+                    EndDate = new DateTime(2020, 3, 18)
+                },
+                new Course
+                {
+                    Name = "My code",
+                    Description = "Code course",
+                    Price = 123,
+                    StartDate = DateTime.Now,
+                    EndDate = new DateTime(2019, 12, 18)
+                },
+                new Course
+                {
+                    Name = "Course 5",
+                    Description = "Just another course",
+                    Price = 50,
+                    StartDate = DateTime.Now,
+                    EndDate = new DateTime(2020, 1, 11)
+                },
+                new Course
+                {
+                    Name = "The Course",
+                    Description = "The best and the most important course",
+                    Price = 100000,
+                    StartDate = DateTime.Now,
+                    EndDate = new DateTime(2019, 10, 1)
+                },
+            };
+
+            context.Courses.AddRange(courses);
+            context.SaveChanges();
+        }
+    }
+}
